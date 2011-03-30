46d9db6df016735a5deeb296c53c3bd0e955fc15
Ramprasad Rajendran <r.ramprasad@gmail.com>
2011-03-30 15:07:28 +0530

    removed gearman folder - not used

--- gearman/gearman_cl1.py
+++
@@ -1,6 +0,0 @@
-from gearman import GearmanClient,Task
-
-client = GearmanClient(["127.0.0.1"])
-#res = client.do_task(Task("echo", "foo"))
-res = client.dispatch_background_task("echo", "foo")
-print res
